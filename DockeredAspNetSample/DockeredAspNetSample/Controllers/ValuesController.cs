8f01a2793088e59ee685d04b0f6b2d5b66aabbf4
Eugene Petrovich <zhpetrovich@yahoo.com>
2020-01-22 20:23:21 +0300

    Added DockeredAspNetSample solution

---
+++ DockeredAspNetSample/DockeredAspNetSample/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockeredAspNetSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var client = new HttpClient();
+            var result = client.GetAsync("http://google.com").Result.Content.ReadAsStringAsync().Result;
+            return new string[] { "value1", "value2", result };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
